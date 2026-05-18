653f9caeb12521bde668f3c150098b8fafff7f54
Januuu26 <b77286116@gmail.com>
2026-05-18 14:56:00 +0700

    mrmbuat interger

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -35,9 +35,14 @@ class budi : public pekerja, public pelajar {
 public:
     budi(int pUmur) :
         pekerja(pUmur),
-        pelajar(pUmur),
-        orang(pUmur)
+        pelajar(pUmur)
     {
         cout << "budi dibuat\n" << endl;
     }
 };
+
+int main() {
+    budi a(12);
+
+    return 0;
+}
