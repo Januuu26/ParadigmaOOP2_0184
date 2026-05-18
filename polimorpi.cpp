da779cce26d3066142e48bb9235f5e65779deee9
Januuu26 <b77286116@gmail.com>
2026-05-18 15:07:31 +0700

    membuat class seseorang

---
+++ polimorpi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0; // pure virtual function
+    // virtual void pesan() {
+    //     cout << "Hallo saya seseorang\n" << endl;
+    // }
+
+};
+
